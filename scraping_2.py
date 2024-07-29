b09179efa0e9f38da837324c55b0efd13603e6b0
fat83dotcom <fernando.quimic@gmail.com>
2024-07-29 14:06:11 -0300

    Funcções para contar chaves do dicionario e função principal.]

--- scraping_2.py
+++ scraping_2.py
@@ -53,28 +53,39 @@ def word_analitics(content: list) -> dict:
 
 
 def word_counter(content: dict) -> int:
-    total_words = 0
-    for k, v in enumerate(content.items()):
-        total_words += v
+    '''Conta o total de ocorrencias de strings em um dicionario.'''
+    words = 0
+    for _, v_count in content.items():
+        words += v_count
 
-    return total_words
+    return words
+
+
+def counter_distinct_words(content: dict) -> int:
+    return len(content)
+
+
+def core(url: str) -> dict:
+    links = request_content(url)
+
+    links_first_page = [
+        link.get('href') for link in find_all_for_tag(links, 'a')
+    ]
+
+    words: list = []
+    for link in links_first_page:
+        bs_obj = request_content(link)
+        if bs_obj is not None:
+            words += word_separator(find_tag_content(bs_obj.body))
+
+    return word_analitics(words)
 
 
 # url = 'https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal'
 url = 'https://www.w3schools.com/'
 
-links = request_content(url)
 
-links_first_page = [
-    link.get('href') for link in find_all_for_tag(links, 'a')
-]
-
-total_words: list = []
-for link in links_first_page:
-    bs_obj = request_content(link)
-    if bs_obj is not None:
-        total_words += word_separator(find_tag_content(bs_obj.body))
-
-result = word_analitics(total_words)
-total_words = word_counter(result)
-print(total_words)
+result = core(url)
+print(result)
+print(word_counter(result))
+print(counter_distinct_words(result))
